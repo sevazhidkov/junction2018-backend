9ede670ef99e0609b7263b29557875b490d49ac6
Alexey Shekhirin <a.shekhirin@gmail.com>
2018-11-23 22:13:00 +0200

    Remove cool function seva sucks

--- messages.py
+++ messages.py
@@ -1,10 +1,6 @@
 from redis import Redis
 import time
 
-messages_dict = {
-    'close_door': 'Someone has forgotten to close the door please fix it boy'
-}
-
 
 def last_message(redis: Redis, user_id):
     while redis.llen(user_id) == 0:
@@ -13,7 +9,5 @@ def last_message(redis: Redis, user_id):
     return redis.lpop(user_id)
 
 
-def save_message(redis: Redis, user_id, message_id, to_format=None):
-    if to_format is None:
-        to_format = []
-    redis.lpush(user_id, messages_dict[message_id].format(*to_format))
+def save_message(redis: Redis, user_id, message):
+    redis.lpush(user_id, message)
