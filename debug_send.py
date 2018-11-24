2814f6d922bc7a92beae00a0ddf22ab0a19430fa
Alexey Shekhirin <a.shekhirin@gmail.com>
2018-11-24 13:18:10 +0200

    Improve debug_send with custom types

--- debug_send.py
+++ debug_send.py
@@ -1,6 +1,14 @@
+import random
 import sys
+import time
+
 from sender import answer_message
 
 type = sys.argv[1]
 
-answer_message(message_type=type)
+if type == 'message':
+    answer_message(message_type=sys.argv[2])
+elif type == 'random':
+    while True:
+        answer_message(message_type=random.choice(['door_open', 'temperature_change', 'too_many_person', 'new_person']))
+        time.sleep(random.randint(1, 20) / 10)
