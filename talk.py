57bc3967ec2d68292bacccaee078e04250a1881b
Seva Zhidkov <v.zhidkov@corp.vk.com>
2018-11-24 18:01:52 +0300

    add talk

---
+++ talk.py
@@ -0,0 +1,34 @@
+import os
+import dialogflow_v2 as dialogflow
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './google_creds.json'
+
+def detect_intent_texts(project_id, session_id, texts, language_code='en'):
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session))
+
+    for text in texts:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+
+        print('=' * 20)
+        print('Query text: {}'.format(response.query_result.query_text))
+        print('Detected intent: {} (confidence: {})\n'.format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence))
+        print('Fulfillment text: {}\n'.format(
+            response.query_result.fulfillment_text))
+
+
+def analyze_message(text):
+    pass
+
+
+detect_intent_texts('sauna-v3', 0, ['how are you', 'karaoke please'])
