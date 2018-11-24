9a6d31570ff422878619732344d1561d68a9d758
Alexey Shekhirin <a.shekhirin@gmail.com>
2018-11-24 13:27:32 +0200

    Flexible type choosing in debug_send

--- debug_send.py
+++ debug_send.py
@@ -2,6 +2,7 @@ import random
 import sys
 import time
 
+from anomaly_detect import sensors
 from sender import answer_message
 
 type = sys.argv[1]
@@ -10,5 +11,5 @@ if type == 'message':
     answer_message(message_type=sys.argv[2])
 elif type == 'random':
     while True:
-        answer_message(message_type=random.choice(['door_open', 'temperature_change', 'too_many_person', 'new_person']))
+        answer_message(message_type=random.choice([x['message_type'] for x in sensors]))
         time.sleep(random.randint(1, 20) / 10)
