e3fd774ac302436437dcd326f6d3affd6b2ef09d
Alexey Shekhirin <a.shekhirin@gmail.com>
2018-11-24 12:34:35 +0200

    Add last_message_handler Add mesaure_handler returning all measurements

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from redis import Redis
 from measures import measurements
+import messages
 
 app = Flask(__name__)
 redis = Redis(db=1)
@@ -13,10 +14,14 @@ def main_handler():
     return jsonify({'success': True})
 
 
-@app.route('/measure')
+@app.route('/measurements')
 def measure_handler():
-    measurement = request.args.get('name')
-    return jsonify({'value': measurements[measurement]()})
+    return jsonify({x: y() for x, y in measurements.item()})
+
+
+@app.route('/last_message')
+def last_message_handler():
+    return messages.last_message(redis, 0)
 
 
 if int(os.environ.get('DEBUG', 1)) == 1:
