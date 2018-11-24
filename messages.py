4dca5de878d6cfc765f37bc24218fe92f38a3f6f
Alexey Shekhirin <a.shekhirin@gmail.com>
2018-11-24 15:29:52 +0200

    Increase sleep to lower the fucking of redis

--- messages.py
+++ messages.py
@@ -4,7 +4,7 @@ import time
 
 def last_message(redis: Redis, user_id):
     while redis.llen(user_id) == 0:
-        time.sleep(0.01)
+        time.sleep(0.1)
         continue
     return redis.lpop(user_id)
 
