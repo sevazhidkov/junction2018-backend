419582c9fef2e22578ee071962c07ac048c5a37e
Alexey Shekhirin <a.shekhirin@gmail.com>
2018-11-24 15:58:45 +0200

    Increase sleep in random sending debug_send

--- debug_send.py
+++ debug_send.py
@@ -12,4 +12,4 @@ if type == 'message':
 elif type == 'random':
     while True:
         answer_message(message_type=random.choice([x['message_type'] for x in sensors]))
-        time.sleep(random.randint(1, 20) / 10)
+        time.sleep(random.randint(1, 5))
